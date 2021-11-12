6cc302106a7ae08c817c5f33568336a937dc45ab
KJI-DEC <kji_dec22@ewhain.net>
2021-11-12 23:36:01 +0900

    [Week2] Update body - character page design

--- Jiin/first_flutter_study/lib/main.dart
+++ Jiin/first_flutter_study/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart'; //flutter framework에 있는 기능을 사용하기 위해 무조건 import 해야함
 
 void main() => runApp(MyApp());
@@ -15,36 +17,51 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "First Flutter App", //app을 총칭하는 이름(앱 이름 그 자체)
-      theme: ThemeData(primarySwatch: Colors.blue //파란색 색상 견본 사용
-          ),
-      home: MyHomePage(),
+      home: Grade(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class Grade extends StatelessWidget {
   //MyApp 안의 자식위젯
-  const MyHomePage({Key? key}) : super(key: key);
+  const Grade({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Jiin\'s first app'), //app 화면에 출력되는 title, appbar에 표시되는 것
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-        elevation: 0.0, //appBar가 뜨는 정도 설정 - 0.0은 화면에 딱 붙어서 나오게 함
-      ),
-      body: Center(
-        //widget 가로축의 정중앙 배치
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment
-              .center, //위젯을 세로축으로 상,중,하단 배치 - center이므로 세로축 가운데에 배치
-          children: <Widget>[
-            Text("My First App"),
-          ],
+        backgroundColor: Colors.amber[800],
+        appBar: AppBar(
+          title: Text('Jiin\'s first app'), //app 화면에 출력되는 title, appbar에 표시되는 것
+          backgroundColor: Colors.amber[700],
+          centerTitle: true,
+          elevation: 0.0,
         ),
-      ),
-    );
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 0.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start, //시작점 정렬
+            children: <Widget>[
+              Text(
+                'Name',
+                style: TextStyle(
+                  color: Colors.white,
+                  letterSpacing: 2.0, //글자 간격 조절
+                ),
+              ),
+              SizedBox(
+                //sized box를 이용해 두 텍스트 사이의 간격 조절
+                height: 10.0,
+              ),
+              Text(
+                "Jiin\'s App",
+                style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ));
   }
 }
